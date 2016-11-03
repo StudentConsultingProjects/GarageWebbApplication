7beb2e68bdd8ba803182ef69e17630ca77646289
Kristoffer <krillllind@gmail.com>
2016-11-03 09:18:13 +0100

    Initial commit

---
+++ GarageWebbApplication/scripts/core.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+    $(document).on("click", ".toggleScroll", function (e) {
+        var el = $(this).data("target");
+        console.log(el);
+
+        $('html, body').animate({
+            scrollTop: ($(el).offset().top) - ($(".navbar").eq(0).height()) 
+        }, 400);
+        
+    });
+
+    setTimeout(function () {
+        var amount = 35;
+        $(".hello-message").css({top: (amount + 10 +"%") });
+        $(".hello-message").animate({
+            opacity: 1,
+            top: (amount + "%")
+        }, 1000);
+    }, 600);
+});
